678aa2ed3f5cdfba08b9f087c2852c8153b78d1c
goleee <golee.dev@gmail.com>
2025-06-05 22:36:07 +0100

    fix: fix command 3

--- remix.config.js
+++ remix.config.js
@@ -1,11 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  server: false,
   appDirectory: 'app',
   assetsBuildDirectory: 'public/build',
   publicPath: '/build/',
   ignoredRouteFiles: ['**/.*'],
-  future: {
-    v2_routeConvention: true,
-  },
 };
